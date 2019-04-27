6f9f9a77c3de5aa728c9f44165c6e90407e60556
patjennings <bonjour@thomasguesnon.fr>
2019-04-27 12:51:32 +0200

    Folder structure modifications

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './client/js/app.js',
+    entry: './src/js/app.js',
     output: {
 	path: path.resolve(__dirname, 'dist'),
 	filename: 'bundle.js'
@@ -18,11 +19,21 @@ module.exports = {
             {
 		test: /\.scss$/,
 		use: [
-                    "style-loader", // creates style nodes from JS strings
+		    // fallback to style-loader in development
+                    process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
                     "css-loader", // translates CSS into CommonJS
                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
 		]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+	    path: path.resolve(__dirname, 'dist'),
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 };
