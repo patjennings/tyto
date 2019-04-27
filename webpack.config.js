144e7c0e95ccccba9162e9449b00c66c063c070d
patjennings <bonjour@thomasguesnon.fr>
2019-04-27 14:46:30 +0200

    Webpack now compiles everything

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: ['./src/js/app.js', './src/sass/main.scss'],
     output: {
 	path: path.resolve(__dirname, 'dist'),
 	filename: 'bundle.js'
@@ -20,20 +20,31 @@ module.exports = {
 		test: /\.scss$/,
 		use: [
 		    // fallback to style-loader in development
-                    process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader", // translates CSS into CommonJS
+                    // process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+		    MiniCssExtractPlugin.loader,
+		    "css-loader", // translates CSS into CommonJS
                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
 		]
+            },
+	    {
+		test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+		use: [{
+                    loader: 'file-loader',
+                    options: {
+			name: '[name]?name=[path][name].[ext]&context=/src/assets/fonts',
+			outputPath: 'assets/fonts/'
+                    }
+		}]
             }
+
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-	    path: path.resolve(__dirname, 'dist'),
-            filename: "[name].css",
-            chunkFilename: "[id].css"
+            filename: "css/[name].css",
+            chunkFilename: "css/[id].css"
         })
     ]
 };
